e08e895f6d7b7efee10f4b5e5dc9f9c12b2a2db8
Safwan Ahmad <safwanahmad787@gmail.com>
2024-10-16 19:44:00 +0600

    first commit

---
+++ Abstraction.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Absclass(ABC):
+
+    def print(self, x):
+        print("Passed Value:", x)
+
+    #Abstract Method
+    @abstractmethod
+    def task(self):
+        print("We are in Absclass task")
+
+#Create subclass
+class test(Absclass):
+    def task(self):
+        print("We are inside test_class task")
+
+#Object of test class created
+testObj = test()
+testObj.task()
+testObj.print(100)
+
+o1 = Absclass
+o1.task()
